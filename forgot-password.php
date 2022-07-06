d6760cc3a79a5d5d06ab3db9a36c1553b4dd1b30
lpjunior <prof.lpjunior@gmail.com>
2022-07-05 21:49:30 -0300

    Add files via upload

--- forgot-password.php
+++ forgot-password.php
@@ -4,29 +4,26 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login SGA</title>
+    <title>Recupera Senha | SGA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
   <body>
     <div class="col-6 offset-3">
     <fieldset>
-            <legend>Login SGA</legend>
-            <form action="loginSistema.php" method="post" class="form">
+            <legend>Recupera senha | SGA</legend>
+            <form action="recupera-senha.php" method="post" class="form">
                 <div class="mb-3 form-group">
                     <label for="emailId" class="form-label">E-mail</label>
                     <input type="email" name="email" id="emailId" class="form-control" placeholder="Informe o e-mail">
                     <div id="helperEmail" class="form-text">Informe o e-mail</div>
                 </div>
-                <div class="mb-3 form-group">
-                    <label for="senhaId" class="form-label">Senha</label>
-                    <input type="password" name="senha" id="senhaId" class="form-control" placeholder="Informe a senha">
-                    <div id="helperSenha" class="form-text">Informe a senha</div>
-                </div>
                 <div class="d-grid gap-2 d-md-block">
-                  <button type="submit" class="btn btn-dark">Login</button>
-                  <a href="forgot-password.php">Recuperar senha</a>
+                  <button type="submit" class="btn btn-dark">Enviar</button>
+                  <a href="index.php">Fazer login</a>
                 </div>
-                <div id="notify" class="form-text text-capitalize fs-4"><?= isset($_COOKIE['notify']) ? $_COOKIE['notify'] : '' ?></div>
+                <?php if(isset($_COOKIE['notify'])) : ?>
+                <div id="notify" class="form-text text-capitalize text-<?= $_COOKIE['status'] ?> fs-4"><?= $_COOKIE['notify'] ?></div>
+                <?php endif; ?>
             </form>
         </fieldset>
     </div>
