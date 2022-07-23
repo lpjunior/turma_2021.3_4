fcc45746e4fa525d4a00f6703b3b0b66729af6ba
lpjunior <prof.lpjunior@gmail.com>
2022-07-22 23:40:07 -0300

    feat: correção do método de test getToDo e método editToDo pendente

--- src/main/java/br/com/senac/todo/controllers/ToDoController.java
+++ src/main/java/br/com/senac/todo/controllers/ToDoController.java
@@ -4,10 +4,7 @@ import br.com.senac.todo.model.ToDo;
 import br.com.senac.todo.services.ToDoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,8 +22,34 @@ public class ToDoController {
         return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping("/todo/{id}")
+    ResponseEntity<ToDo> getToDo(@PathVariable long id) {
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
     @PostMapping("/todo")
     ResponseEntity<ToDo> create(@RequestBody ToDo toDo) {
         return new ResponseEntity<>(service.save(toDo), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/todos")
+    ResponseEntity<List<ToDo>> create(@RequestBody List<ToDo> toDos) {
+        return new ResponseEntity<>(service.saveAll(toDos), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/todo")
+    ResponseEntity<ToDo> update(@RequestBody ToDo toDo) {
+        return new ResponseEntity<>(service.save(toDo), HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/todos")
+    ResponseEntity<List<ToDo>> update(@RequestBody List<ToDo> toDos) {
+        return new ResponseEntity<>(service.saveAll(toDos), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("todo/{id}")
+    ResponseEntity delete(@PathVariable long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/test/java/br/com/senac/todo/controllers/ToDoControllerTest.java
+++ src/test/java/br/com/senac/todo/controllers/ToDoControllerTest.java
@@ -2,6 +2,7 @@ package br.com.senac.todo.controllers;
 
 import br.com.senac.todo.model.ToDo;
 import br.com.senac.todo.services.ToDoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,13 +13,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
@@ -37,22 +40,75 @@ public class ToDoControllerTest {
         toDoList.add(new ToDo(2L, "Dormir PELO MENOS duas vezes", true));
         Mockito.when(toDoService.findAll()).thenReturn(toDoList);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/todos").contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/todos").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", Matchers.hasSize(2))).andDo(print());
     }
-
-    // finalizamos a cobertura de testes da nossa classe service
-    // finalizamos a aula com um erro no test de getToDo para efetuar a correção minimalista na próxima aula.
-
-    // Atividade de vocês: Resolver o problema getToDo
-    // +++ implementar os testes para edit, save e delete
 
     @Test
     void getToDo() throws Exception {
         var toDo = new ToDo(1L, "Comer três vezes", true);
-        Mockito.when(toDoService.findById(toDo.getId())).thenReturn(toDo);
+        Mockito.when(toDoService.findById(any(Long.class))).thenReturn(toDo);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/todo/1").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", Matchers.hasSize(1))).andDo(print());
+        mockMvc.perform(
+                    get("/todo/1").contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(toDo), false))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    void createToDo() throws Exception {
+        var toDo = new ToDo(1L, "Entrar em férias", false);
+        Mockito.when(toDoService.save(any(ToDo.class))).thenReturn(toDo);
+
+        var mapper = new ObjectMapper();
+        var jsonToDo = mapper.writeValueAsString(toDo);
+
+        var resultActions = mockMvc.perform(
+                post("/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonToDo)
+        );
+
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(content().json(jsonToDo))
+                .andExpect(jsonPath("$.description").value("Entrar em férias"));
+    }
+
+    // finalizamos a aula com correção no test de getToDo.
+    // finalizamos o teste createToDo
+    // correção da implementação do teste para edit
+
+    @Test
+    void editToDo() throws Exception {
+        var toDo = new ToDo(1L, "Entrar em férias", false);
+        Mockito.when(toDoService.save(any(ToDo.class))).thenReturn(toDo);
+
+        var mapper = new ObjectMapper();
+        var jsonToDo = mapper.writeValueAsString(toDo);
+
+        var resultActions = mockMvc.perform(
+                post("/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonToDo)
+        );
+
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(content().json(jsonToDo))
+                .andExpect(jsonPath("$.description").value("Entrar em férias"));
+    }
+
+    @Test
+    void deleteToDo() throws Exception {
+        var todoServiceMock = mock(ToDoService.class);
+        doNothing().when(todoServiceMock).deleteById(any(Long.class));
+
+        mockMvc.perform(
+                        delete("/todo/1").contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNoContent());
     }
 }
