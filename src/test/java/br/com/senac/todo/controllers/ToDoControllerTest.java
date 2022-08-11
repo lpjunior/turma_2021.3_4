8bc7b396e93f9c01137ba540d0b91922a3130587
lpjunior <prof.lpjunior@gmail.com>
2022-08-10 21:38:58 -0300

    feat: correção do exercicio

--- src/test/java/br/com/senac/todo/controllers/ToDoControllerTest.java
+++ src/test/java/br/com/senac/todo/controllers/ToDoControllerTest.java
@@ -18,7 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- src/test/java/br/com/senac/todo/webdriver/TestWebBrowser.java
+++ src/test/java/br/com/senac/todo/webdriver/TestWebBrowser.java
@@ -8,15 +8,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestWebBrowser {
 
     private WebDriver driver;
     private WebElement searchBox;
+    private WebElement searchButton;
     private WebElement searchResults;
+
+    private Actions actions;
 
     @BeforeEach
     void openBrowser() {
@@ -43,7 +53,29 @@ public class TestWebBrowser {
         // resultado
         searchResults = driver.findElement(By.cssSelector("#search"));
 
+        System.out.println("driver.findElement(By.tagName(\"title\")) = " + driver.findElement(By.tagName("title")));
         assertTrue(searchResults.isDisplayed());
         assertThat(driver.getTitle().startsWith("Senac Rio"));
+    }
+
+    @Test
+    void searchClass() {
+        driver.get("https://www.rj.senac.br");
+
+        searchButton = driver.findElement(By.id("bt-opensearch"));
+
+        actions = new Actions(driver);
+        actions.moveToElement(searchButton).click().perform();
+
+        searchBox = new WebDriverWait(driver, Duration.of(10, ChronoUnit.SECONDS)).until(ExpectedConditions.elementToBeClickable(By.id("search-field")));
+        searchBox.sendKeys("Técnico em informática");
+        searchBox.submit();
+
+        searchButton = driver.findElement(By.className("thumb-busca"));
+        actions.moveToElement(searchButton).click().perform();
+
+        var turma = driver.findElement(By.cssSelector("h1[class='fw-bold color-2-1 flt-left']")).getText();
+
+        assertEquals("Turma 2022.3", turma);
     }
 }
