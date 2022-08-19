46a55c8e98a76283bd470a3a11a3f7aef406e9e5
Luis Lessa <prof.lpjunior@gmail.com>
2022-08-18 22:22:02 -0300

    feat: criação do projeto

---
+++ src/projetoCRUDbasico/app/App.java
@@ -0,0 +1,9 @@
+package projetoCRUDbasico.app;
+
+// FQN "Full Qualified Name: projetoCRUDbasico.app.App
+public class App {
+    public static void main(String[] args) {
+        Aluno alu; // referencia a classe Aluno
+        alu = new Aluno(); // criando uma nova instancia e atribuindo o endereço de memória dessa instancia para a referencia
+    }
+}
---
+++ src/projetoCRUDbasico/model/Aluno.java
@@ -0,0 +1,16 @@
+package projetoCRUDbasico.model;
+
+public class Aluno {
+    public String nome;
+    private String email;
+    protected int matricula;
+    String sexo;
+
+    public void assistirAula() {
+        System.out.println("O aluno está assistindo a aula de Java");
+    }
+
+    public String efetuarPergunta() {
+        return "O aluno está fazendo uma pergunta sobre duvida dele de Java";
+    }
+}
