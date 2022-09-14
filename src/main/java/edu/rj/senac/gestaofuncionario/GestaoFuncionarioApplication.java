99bda6baf0802589ab484be1959e60dcfa9e6ce1
Luis Lessa <prof.lpjunior@gmail.com>
2022-09-13 21:42:32 -0300

    feat: criação do projeto

---
+++ src/main/java/edu/rj/senac/gestaofuncionario/GestaoFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package edu.rj.senac.gestaofuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFuncionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoFuncionarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/rj/senac/gestaofuncionario/GestaoFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rj.senac.gestaofuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
